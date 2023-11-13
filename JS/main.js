9d08a5cbaf279c53e6f8ea08a04496cc683a5a39
Joao Rampo <joao.rampoo@gmail.com>
2023-11-13 17:41:14 -0300

    mask setup

---
+++ JS/main.js
@@ -0,0 +1,13 @@
+$(document).ready(function () {
+  $("#telefone").mask("(00) 00000-0000");
+
+  $("#cep").mask("00000-000");
+
+  $("#cpf").mask("000.000.000-00");
+
+  $("button").click(function () {
+    const invalidInputs = $("input").val();
+
+    alert(`Existem ${invalidInputs} inputs inválidos`);
+  });
+});
