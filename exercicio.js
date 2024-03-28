a066af8aabc49bfa241b1ccc73625f30625266df
Joao <joao.rampoo@gmail.com>
2024-03-28 16:25:58 -0300

    exercicio array objeto

---
+++ exercicio.js
@@ -0,0 +1,18 @@
+const alunos = [
+    {nome: 'joao', nota: 10},
+    {nome: 'maria', nota: 8},
+    {nome: 'roger', nota: 4},
+    {nome: 'kleber', nota: 6},
+    {nome: 'kleiton', nota: 5}
+]
+
+function filtraNota6(arrayAlunos) {
+    return arrayAlunos
+    .filter(aluno => (aluno.nota >= 6))
+    .map(aluno => aluno.nome)
+}
+
+const filtradosNota = filtraNota6(alunos)
+
+console.log(`Os alunos: ${filtradosNota} foram aprovados!`)
+
