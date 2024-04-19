b14a4eea09866c330fd99ea294c0da2741745158
Joao Rampo <joao.rampoo@gmail.com>
2024-04-19 16:44:36 -0300

    exercicio - erro ao rodar o scrpt

---
+++ exercicio.ts
@@ -0,0 +1,10 @@
+function multiply(a: number, b: number): number {
+  return a * b;
+}
+
+function saudacao(nome: string): string {
+  return `Olá ${nome}`;
+}
+
+console.log(multiply(5, 2));
+console.log(saudacao("João"));
